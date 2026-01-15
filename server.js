32adfc233233ef6a89f4715f61197d22654208f5
Jaime Sanchez <jaimesanchez@Portatil-Jaime.local>
2026-01-15 22:16:02 +0100

    Bien

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const API_KEY = process.env.API_KEY;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rate limiting
 let lastRequestTime = 0;
